9ad40b3fb297d405d53df97613a104507a952ce8
Ezhevikina <ezhevikina@Ezhevikinas-MacBook-Pro.local>
2018-09-16 23:08:35 +0300

    Change formatting settings

--- sandbox/src/main/java/pack/pdt/sandbox/HelloJava.java
+++ sandbox/src/main/java/pack/pdt/sandbox/HelloJava.java
@@ -1,7 +1,9 @@
 package pack.pdt.sandbox;
 
 public class HelloJava {
-	public static void main(String[] args) {
-		System.out.println("Hello, world!");
-	}
+
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
+  }
+
 }
