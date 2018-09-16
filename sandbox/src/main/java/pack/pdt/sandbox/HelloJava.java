daf9c4b9923402cea392d68181b939a33b977a92
Ezhevikina <ezhevikina@gmail.com>
2018-09-17 01:21:19 +0300

    Add some functions for example

--- sandbox/src/main/java/pack/pdt/sandbox/HelloJava.java
+++ sandbox/src/main/java/pack/pdt/sandbox/HelloJava.java
@@ -3,8 +3,27 @@ package pack.pdt.sandbox;
 public class HelloJava {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    hello("User");
+    hello("oh cruel world");
 
+    double length = 5;
+    System.out.println("Area of a square with side length " + length + " = " + area(length));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Area of a rectangle with side lengths " + a + " and " + b + " = " + area(a, b));
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area(double l) {
+    return l * l;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
   }
 
 }
