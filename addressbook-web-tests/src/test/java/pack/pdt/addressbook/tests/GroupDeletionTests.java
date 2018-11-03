4decd301b091d81ed16fca2dadc625bde0e0c9c6
Ezhevikina <ezhevikina@gmail.com>
2018-11-03 20:47:51 +0300

    Check and assure group existense before deletion

--- addressbook-web-tests/src/test/java/pack/pdt/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/pack/pdt/addressbook/tests/GroupDeletionTests.java
@@ -1,12 +1,16 @@
 package pack.pdt.addressbook.tests;
 
 import org.testng.annotations.*;
+import pack.pdt.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
   @Test
   public void testGroupDeletion() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().groupExists()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
