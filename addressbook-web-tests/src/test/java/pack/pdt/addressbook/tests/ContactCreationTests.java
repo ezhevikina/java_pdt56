c4d4ad9472e4442872d5da24b15a455d962ca621
Ezhevikina <ezhevikina@gmail.com>
2018-11-03 21:27:39 +0300

    Check and assure contact existance before modification or deletion

--- addressbook-web-tests/src/test/java/pack/pdt/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/pack/pdt/addressbook/tests/ContactCreationTests.java
@@ -8,11 +8,8 @@ public class ContactCreationTests extends TestBase {
   @Test
   public void testCreationContact() throws Exception {
     app.getNavigationHelper().gotoAddNewContactPage();
-    app.getContactHelper().fillContactForm(new ContactData("John", "Doe",
+    app.getContactHelper().createContact(new ContactData("John", "Doe",
             "Software Testing Company", "Moscow", "89000000001",
             "johnd@stc.com", "test1"), true);
-    app.getContactHelper().submitContactCreation();
-    app.getNavigationHelper().gotoToHomePage();
   }
-
 }
--- addressbook-web-tests/src/test/java/pack/pdt/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/pack/pdt/addressbook/tests/ContactDeletionTests.java
@@ -1,11 +1,18 @@
 package pack.pdt.addressbook.tests;
 
 import org.testng.annotations.Test;
+import pack.pdt.addressbook.model.ContactData;
 
 public class ContactDeletionTests extends TestBase {
 
   @Test
   public void testContactDeletion() throws Exception {
+    if (! app.getContactHelper().contactExists()) {
+      app.getNavigationHelper().gotoAddNewContactPage();
+      app.getContactHelper().createContact(new ContactData("John", null,
+              null, null, null,
+              "johnd@stc.com", "test1"), true);
+    }
     app.getContactHelper().selectContact();
     app.getContactHelper().deleteContact();
     app.getContactHelper().submitContactDeletion();
