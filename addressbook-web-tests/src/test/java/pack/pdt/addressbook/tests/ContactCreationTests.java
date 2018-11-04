5df6f174b4abf084f5ff38f880334ee840a582e2
Ezhevikina <ezhevikina@gmail.com>
2018-11-04 19:26:18 +0300

    Compare contact list before and after contact creation

--- addressbook-web-tests/src/test/java/pack/pdt/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/pack/pdt/addressbook/tests/ContactCreationTests.java
@@ -4,16 +4,26 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import pack.pdt.addressbook.model.ContactData;
 
+import java.util.Comparator;
+import java.util.List;
+
 public class ContactCreationTests extends TestBase {
 
   @Test
   public void testCreationContact() throws Exception {
-    int before = app.getContactHelper().getContactCount();
+    List<ContactData> before = app.getContactHelper().getContactList();
     app.getNavigationHelper().gotoAddNewContactPage();
-    app.getContactHelper().createContact(new ContactData("John", "Doe",
+    ContactData contact = new ContactData("John", "Doe",
             "Software Testing Company", "Moscow", "89000000001",
-            "johnd@stc.com", "test1"), true);
-    int after = app.getContactHelper().getContactCount();
-    Assert.assertEquals(after,before + 1);
+            "johnd@stc.com", "test1");
+    app.getContactHelper().createContact(contact, true);
+    List<ContactData> after = app.getContactHelper().getContactList();
+
+    Assert.assertEquals(after.size(),before.size() + 1);
+    before.add(contact);
+    Comparator<? super ContactData> byId = Comparator.comparingInt(ContactData::getId);
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before, after);
   }
 }
