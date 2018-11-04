8d31213df4722c9a025c5d4322d78abd7bf28a3f
Ezhevikina <ezhevikina@gmail.com>
2018-11-04 23:19:32 +0300

    Make group tests fluent

--- addressbook-web-tests/src/test/java/pack/pdt/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/pack/pdt/addressbook/tests/GroupCreationTests.java
@@ -3,9 +3,7 @@ package pack.pdt.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pack.pdt.addressbook.model.GroupData;
-
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase {
@@ -14,13 +12,15 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() throws Exception {
     app.goTo().groupPage();
     List<GroupData> before = app.group().list();
-    GroupData group = new GroupData("test2", null, null);
+    GroupData group = new GroupData().withName("test2");
     app.group().create(group);
     List<GroupData> after = app.group().list();
 
     Assert.assertEquals(after.size(), before.size() + 1);
-    group.setId(after.stream().max(Comparator.comparingInt(GroupData::getId)).get().getId());
     before.add(group);
-    Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
+    Comparator<? super GroupData> byId = Comparator.comparingInt(GroupData::getId);
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before, after);
   }
 }
