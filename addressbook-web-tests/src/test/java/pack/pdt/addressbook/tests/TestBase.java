b381a1dd45c6618fc528e159a0072004372b8157
Ezhevikina <ezhevikina@gmail.com>
2018-11-11 12:22:30 +0300

    Use logback lib to write logs to log file

--- addressbook-web-tests/src/test/java/pack/pdt/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/pack/pdt/addressbook/tests/TestBase.java
@@ -1,11 +1,21 @@
 package pack.pdt.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import pack.pdt.addressbook.appmanager.ApplicationManager;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class TestBase {
+
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
 
   protected static final ApplicationManager app
           = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
@@ -18,6 +28,17 @@ public class TestBase {
   @AfterSuite(alwaysRun = true)
   public void tearDown() throws Exception {
     app.stop();
+  }
+
+  @BeforeMethod
+  public void logTestStart(Method method, Object[] parameters) {
+    logger.info("Start test " + method.getName()
+            + " with parameters" + Arrays.asList(parameters));
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void logTestStop(Method method) {
+    logger.info("Stop test " + method.getName());
   }
 
 }
